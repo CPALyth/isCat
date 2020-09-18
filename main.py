6e9157d8928eb4c992bcee0d853bb6006603d902
CPALyth <160548528@qq.com>
2020-09-18 15:56:01 +0800

    v0.2

--- main.py
+++ main.py
@@ -50,10 +50,10 @@ test_set_x = test_set_x_flatten / 255
 sigmoid = lambda x: 1. / (1 + np.exp(-x))
 
 def initialize_with_zeros(dim):
-    w = np.zeros(shape = (dim,1))
+    w = np.zeros(shape = (1, dim))
     b = 0
     #使用断言来确保我要的数据是正确的
-    assert(w.shape == (dim, 1)) #w的维度是(dim,1)
+    assert(w.shape == (1, dim)) #w的维度是(1, dim)
     assert(isinstance(b, float) or isinstance(b, int)) #b的类型是float或者是int
 
     return (w , b)
@@ -62,10 +62,10 @@ def propagate(w, b, X, Y):
     """
     实现前向和后向传播的成本函数及其梯度。
     参数：
-        w  - 权重，大小不等的数组（num_px * num_px * 3，1）
+        w  - 权重，大小不等的数组（1，num_px * num_px * 3）
         b  - 偏差，一个标量
         X  - 矩阵类型为（num_px * num_px * 3，训练数量）
-        Y  - 真正的“标签”矢量（如果非猫则为0，如果是猫则为1），矩阵维度为(1,训练数据数量)
+        Y  - 真正的“标签”矢量（如果非猫则为0，如果是猫则为1），矩阵维度为(1,训练数量)
 
     返回：
         cost- 逻辑回归的负对数似然成本
@@ -75,11 +75,11 @@ def propagate(w, b, X, Y):
     m = X.shape[1]
 
     #正向传播
-    A = sigmoid(np.dot(w.T,X) + b) #计算激活值，请参考公式2。
+    A = sigmoid(np.dot(w, X) + b) #计算激活值，请参考公式2。   矩阵维度为(1,训练数量)
     cost = (- 1 / m) * np.sum(Y * np.log(A) + (1 - Y) * (np.log(1 - A))) #计算成本，请参考公式3和4。
 
     #反向传播
-    dw = (1 / m) * np.dot(X, (A - Y).T) #请参考视频中的偏导公式。
+    dw = (1 / m) * np.dot((A - Y), X.T) #请参考视频中的偏导公式。
     db = (1 / m) * np.sum(A - Y) #请参考视频中的偏导公式。
 
     #使用断言确保我的数据是正确的
@@ -88,7 +88,6 @@ def propagate(w, b, X, Y):
     cost = np.squeeze(cost)
     assert(cost.shape == ())
 
-    aaa=1
     #创建一个字典，把dw和db保存起来。
     grads = {
                 "dw": dw,
@@ -101,7 +100,7 @@ def optimize(w , b , X , Y , num_iterations , learning_rate , print_cost = False
     此函数通过运行梯度下降算法来优化w和b
 
     参数：
-        w  - 权重，大小不等的数组（num_px * num_px * 3，1）
+        w  - 权重，大小不等的数组（1，num_px * num_px * 3）
         b  - 偏差，一个标量
         X  - 维度为（num_px * num_px * 3，训练数据的数量）的数组。
         Y  - 真正的“标签”矢量（如果非猫则为0，如果是猫则为1），矩阵维度为(1,训练数据的数量)
@@ -129,8 +128,8 @@ def optimize(w , b , X , Y , num_iterations , learning_rate , print_cost = False
         dw = grads["dw"]
         db = grads["db"]
 
-        w = w - learning_rate * dw
-        b = b - learning_rate * db
+        w -= learning_rate * dw
+        b -= learning_rate * db
 
         #记录成本
         if i % 100 == 0:
@@ -152,7 +151,7 @@ def predict(w , b , X ):
     使用学习逻辑回归参数logistic （w，b）预测标签是0还是1，
 
     参数：
-        w  - 权重，大小不等的数组（num_px * num_px * 3，1）
+        w  - 权重，大小不等的数组（1，num_px * num_px * 3）
         b  - 偏差，一个标量
         X  - 维度为（num_px * num_px * 3，训练数据的数量）的数据
 
@@ -163,10 +162,10 @@ def predict(w , b , X ):
 
     m  = X.shape[1] #图片的数量
     Y_prediction = np.zeros((1,m))
-    w = w.reshape(X.shape[0],1)
+    w = w.reshape(1, X.shape[0])
 
     #计预测猫在图片中出现的概率
-    A = sigmoid(np.dot(w.T , X) + b)
+    A = sigmoid(np.dot(w, X) + b)
     for i in range(A.shape[1]):
         #将概率a [0，i]转换为实际预测p [0，i]
         Y_prediction[0,i] = 1 if A[0,i] > 0.5 else 0
--- test.py
+++ test.py
@@ -1,9 +1,114 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import h5py
 
-lst = [1,2,3,4,5,6]
-a1 = np.array(lst)
-print(a1)
-a2 = a1.reshape((6,-1))
-print(a2)
-a3 = np.squeeze(a2)
-print(a3)
+sigmoid = lambda x: 1. / (1 + np.exp(-x))
+
+
+def load_data():
+    train_set = h5py.File('datasets/train_catvnoncat.h5', 'r')
+    train_set_feature = np.array(train_set['train_set_x'])  # (209,64,64,3)
+    train_set_label = np.array(train_set['train_set_y'])  # (209,)    [0 0 1 0 0]
+    train_set_label = train_set_label.reshape((1, train_set_label.shape[0]))  # (1, 209)
+
+    test_set = h5py.File('datasets/test_catvnoncat.h5', 'r')
+    test_set_feature = np.array(test_set['test_set_x'])  # (50,64,64,3)
+    test_set_label = np.array(test_set['test_set_y'])  # (50,) [1 1 1 1 1 0 1...]
+    test_set_label = test_set_label.reshape((1, test_set_label.shape[0]))  # (1, 50)
+
+    classes = np.array(test_set['list_classes'])  # [b'non-cat' b'cat']
+
+    return train_set_feature, train_set_label, test_set_feature, test_set_label, classes
+
+
+# 读取数据
+train_set_feature, train_set_label, test_set_feature, test_set_label, classes = load_data()
+train_set_num = train_set_label.shape[1]
+test_set_num = test_set_label.shape[1]
+num_px = train_set_feature.shape[1]
+# 将训练集的特征列表降维为(64*64*3, 209), 并将颜色值/255, 使得每个元素在[0,1]之间
+train_set_feature = train_set_feature.reshape(train_set_feature.shape[0], -1).T
+train_set_feature = train_set_feature / 255
+# 将特征集的特征列表降维为(64*64*3, 50), 并将颜色值/255, 使得每个元素在[0,1]之间
+test_set_feature = test_set_feature.reshape(test_set_feature.shape[0], -1).T
+test_set_feature = test_set_feature / 255
+
+
+def initWb(dim):
+    W = np.zeros((1, dim))
+    b = 0
+    return (W, b)
+
+
+def propagate(W, b, X, Y):
+    '''
+    实现前向和后向传播的成本函数及其梯度
+    :param W: (1,n0)
+    :param b: 标量
+    :param X: (n0,m)
+    :param Y: (1,m)
+    :return: Tuple-(grad,L)
+    '''
+    m = X.shape[1]
+    # 正向传播
+    A = sigmoid(np.dot(W, X) + b)
+    L = np.sum(-Y * np.log(A) - (1 - Y) * np.log(1 - A)) / m
+    # 反向传播
+    dW = (1 / m) * np.dot(A - Y, X.T)
+    db = (1 / m) * np.sum(A - Y)
+    grad = {'dW': dW, 'db': db}
+    return (grad, L)
+
+
+def optimize(W, b, X, Y, num_iter, lr, isPrint=True):
+    Ls = []
+    for i in range(num_iter):
+        grad, L = propagate(W, b, X, Y)
+        dW = grad['dW']
+        db = grad['db']
+        W = W - lr * dW
+        b = b - lr * db
+        # 每迭代100次,记录1次损失函数的值
+        if (i % 100 == 0):
+            Ls.append(L)
+            if (isPrint):
+                print("迭代的次数: %i ， 误差值： %f" % (i, L))
+
+    Wb = {'W': W, 'b': b}
+    grad = {'dW': dW, 'db': db}
+    return (Wb, grad, Ls)
+
+
+def predict(W, b, X):
+    n0 = X.shape[0]
+    m = X.shape[1]
+    # 初始化 预测结果矩阵Y_hat的初始值为0
+    Y_hat = np.zeros((1, m))
+    W = W.reshape((1, n0))
+    # A.shape = (1,m)
+    A = sigmoid(np.dot(W, X) + b)
+    # 对预测值逻辑回归,即概率>0.5视为1,否则视为0
+    for i in range(m):
+        Y_hat[0, i] = 1 if (A[0, i] > 0.5) else 0
+    return Y_hat
+
+
+def model(train_feature, train_label, test_feature, test_label, num_iter=2000, lr=0.5, isPrint=True):
+    W, b = initWb(train_feature.shape[0])
+    Wb, grad, Ls = optimize(W, b, train_feature, train_label, num_iter, lr, isPrint)
+    W, b = Wb['W'], Wb['b']
+    Y_hat_train = predict(W, b, train_feature)
+    Y_hat_test = predict(W, b, test_feature)
+    print("训练集准确性:", 100 - np.mean(np.abs(Y_hat_train - train_label)) * 100, '%')
+    print("测试集准确性:", 100 - np.mean(np.abs(Y_hat_test - test_label)) * 100, '%')
+    d = {'Ls': Ls, 'Y_hat_train': Y_hat_train, 'Y_hat_test': Y_hat_test, 'W': W, 'b': b, 'lr': lr, 'num_iter': num_iter}
+    return d
+
+
+d = model(train_set_feature, train_set_label, test_set_feature, test_set_label)
+Ls = np.squeeze(d['Ls'])
+plt.plot(Ls)
+plt.xlabel('iteration(per hundreds)')
+plt.ylabel('Losses')
+plt.title('lr=' + str(d['lr']))
+plt.show()
